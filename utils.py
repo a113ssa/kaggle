c5badcb32570343e0a858ec6999c903a2c61d793
a113ssa <95538451+a113ssa@users.noreply.github.com>
2024-10-17 17:50:06 +0400

    Kaggle Notebook | Utils | Version 3

---
+++ utils.py
@@ -0,0 +1,12 @@
+# %% [code]
+# %% [code]
+import numpy as np
+
+def shuffle_and_split_data(data, test_ratio, seed = None):
+    if (seed != None):
+        np.random.seed(seed)
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
