79a2ef3f414020e260c2f47a6ea82dbcfe2a7d13
Kate <95538451+a113ssa@users.noreply.github.com>
2024-11-11 23:28:09 +0400

    Kaggle Notebook | Utils | Version 7

--- utils.py
+++ utils.py
@@ -1,6 +1,35 @@
 # %% [code]
 import os
 
+import matplotlib.pyplot as plt
+from matplotlib.ticker import (MultipleLocator, FormatStrFormatter)
+
+def plot_train_results(metrics, title=None, ylabel=None, ylim=None, metric_name=None, color=None):
+    fix, ax = plt.subplots(figsize=(15, 4))
+    
+    if not (isinstance(metric_name, list) or isinstance(metric_name, tuple)):
+        metrics = [metrics,]
+        metric_name = [metric_name, ]
+        
+    for idx, metric in enumerate(metrics):
+        ax.plot(metric, color=color[idx])
+        
+    plt.xlabel('Epoch')
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.xlim([0, TrainingConfig.EPOCHS - 1])
+    plt.ylim(ylim)
+    
+    ax.xaxis.set_major_locator(MultipleLocator(5))
+    ax.xaxis.set_major_formatter(FormatStrFormatter('%d'))
+    ax.xaxis.set_minor_locator(MultipleLocator(1))
+    
+    plt.grid(True)
+    plt.legend(metric_name)
+    plt.show()
+    plt.close()
+
+
 from zipfile import ZipFile
 from urllib.request import urlretrieve
 
